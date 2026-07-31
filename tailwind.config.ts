d0e33a9741ad12bf4f4ec80b0b29e0b627669d09
alexvyazmin <alex.vyazmin@gmail.com>
2026-07-31 15:36:56 +0500

    доработки

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        milky: "#F9F8F3",
+        "slate-dark": "#2C2E31",
+        accent: "#C45C3E",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
